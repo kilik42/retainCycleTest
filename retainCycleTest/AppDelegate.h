eae320589f86369c84089c9a20edb8d7a3b0ddb0
marvin evins <maevins@cps.edu>
2016-06-02 18:37:55 -0500

    Initial Commit

---
+++ retainCycleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  retainCycleTest
+//
+//  Created by marvin evins on 6/2/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ retainCycleTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  retainCycleTest
+//
+//  Created by marvin evins on 6/2/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
